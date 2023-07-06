dceca3c7960561e593ff11304943894ce873637d
MZR Development <138665703+MZRDevelopment@users.noreply.github.com>
2023-07-06 14:02:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -48,19 +48,19 @@ const rest = new REST({ version: '10' }).setToken(token);
   }
 })();
 
-client.on('ready', () => {
-  console.log("Bot Aktif!");
-  client.user.setActivity({
-    name: 'YouTube: @MZRDev',
-    type: ActivityType.Playing
+//client.on('ready', () => {
+  //console.log("Bot Aktif!");
+  //client.user.setActivity({
+    //name: 'YouTube: @MZRDev',
+    //type: ActivityType.Playing
     // ActivityType.Playing = oynuyor
     // ActivityType.Watching = izliyor
     // ActivityType.Streaming = Discord sunucuma gelip nasıl yapıldığını öğrenebilirsin
     // ActivityType.Listening = dinliyor
     // ActivityType.Competing = yarışmasında yarışıyor
     // Eğerki dökümanlardan görmek istiyorsanız: https://discord-api-types.dev/api/discord-api-types-v10/enum/ActivityType
-  });
-});
+  //});
+//});
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
@@ -70,4 +70,15 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
-client.login(token); // YouTube: MZR Development
+client.login(token)
+  .then(() => {
+  console.log(`Bot ${client.user.username} ismi ile giriş yaptı!`); // YouTube: MZR Development
+   client.user.setActivity({ name: 'YouTube: @MZRDev', type: ActivityType.Playing })
+    // ActivityType.Playing = oynuyor
+    // ActivityType.Watching = izliyor
+    // ActivityType.Streaming = Discord sunucuma gelip nasıl yapıldığını öğrenebilirsin
+    // ActivityType.Listening = dinliyor
+    // ActivityType.Competing = yarışmasında yarışıyor
+    // Eğerki dökümanlardan görmek istiyorsanız: https://discord-api-types.dev/api/discord-api-types-v10/enum/ActivityType
+  })
+  .catch((err) => console.log(err));
